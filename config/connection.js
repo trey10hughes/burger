017bd413241f6cf61837b5d99cc6637e995bf813
Theodore hughes <theodorehughes@Theodores-MBP.home>
2018-05-02 20:43:34 -0500

    fixed db connection

--- config/connection.js
+++ config/connection.js
@@ -4,23 +4,12 @@ var connection;
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_IRL);
 } else {
-    var connection = mysql.createConnection({
+    connection = mysql.createConnection({
         host: "localhost",
         user: "root",
         password: "",
         database: "burgers_db"
     });
 }
-
-
-
-
-connection.connect(function (err) {
-    if (err) {
-        console.log("connection error: " + err);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
 
 module.exports = connection;
