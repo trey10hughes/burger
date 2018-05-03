627f7ba1533caeb00aa449ce52326d9f36c8b124
Theodore hughes <theodorehughes@Theodores-MBP.home>
2018-05-02 19:33:11 -0500

    functionality complete, needs styling going forward

--- server.js
+++ server.js
@@ -1,4 +1,26 @@
 var express = require('express');
-var methodOverride = require('method-override');
-var bodyParser = require('body-parser');
+var methodoverride = require('method-override');
+var bodyparser = require('body-parser');
 
+var app = express();
+//Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static('public'));
+// app.use(express.static('public'));
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyparser.urlencoded({ extended: false }));
+
+// Handlebars
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+var router = require('./controllers/burgers_controller.js');
+app.use('/', router);
+
+// Open Server
+var port = process.env.PORT || 8080;
+app.listen(port, function () {
+    console.log("App listening on PORT: " + port);
+});
