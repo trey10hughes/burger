5b75404bbd5305f0cbcbf7d1e40ba2f2d3604de8
Theodore hughes <theodorehughes@Theodores-MBP.home>
2018-05-02 20:19:07 -0500

    set up JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,18 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_IRL);
+} else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+}
+
+
 
 
 connection.connect(function (err) {
